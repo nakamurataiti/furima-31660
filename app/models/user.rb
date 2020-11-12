f039900f6f26ab6a23a22cc7c2c0f298e9d5a6e0
nakamurataiti <67047092+nakamurataiti@users.noreply.github.com>
2020-11-12 16:22:18 +0900

    ユーザー管理機能　バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  validates :nickname, presence: true
+  validates :birth_day, presence: true
+
+with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください'} do
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+end
+
+with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カナを使用してください'} do
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+end
+
+VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX, message: 'パスワードには英字と数字の両方を含めて設定してください' }
+
+
 end
