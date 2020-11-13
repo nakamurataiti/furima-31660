93bba6a6f52f8083259d9ff9a28a97205060ca8d
nakamurataiti <67047092+nakamurataiti@users.noreply.github.com>
2020-11-13 17:18:09 +0900

    rubocopの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,24 +2,22 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable
-  
-  
-  VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+         :recoverable, :rememberable, :validatable
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :password, presence: true,
-  format: { with: VALID_PASSWORD_REGEX, message: 'は英字と数字の両方を含めて設定してください' }
+                       format: { with: VALID_PASSWORD_REGEX, message: 'は英字と数字の両方を含めて設定してください' }
 
   validates :nickname,  presence: true
   validates :birth_day, presence: true
 
-with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角文字を使用してください'} do
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-end  
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角文字を使用してください' } do
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+  end
 
-with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'は全角カナを使用してください'} do
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
-end  
-
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'は全角カナを使用してください' } do
+    validates :first_name_kana, presence: true
+    validates :last_name_kana, presence: true
+  end
 end
