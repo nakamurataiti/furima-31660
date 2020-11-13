ee183c363b5644d7ecaf6da1bedf5046a67bcb55
nakamurataiti <67047092+nakamurataiti@users.noreply.github.com>
2020-11-13 17:11:51 +0900

    テストコード

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,117 @@
+require 'rails_helper'
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+      it "全ての項目の入力が存在すれば登録できること" do
+        expect(@user).to be_valid
+      end
+
+   
+
+      it "nicknameがない場合は登録できないこと" do 
+        @user.nickname = nil 
+        @user.valid? 
+        expect(@user.errors[:nickname]).to include("を入力してください") 
+      end
+
+
+
+      it "emailがない場合は登録できないこと" do
+        @user.email = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Eメールを入力してください")
+      end
+
+      it "重複したemailが存在する場合登録できないこと" do
+        @user.save
+        another_user = FactoryBot.build(:user, email: @user.email)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Eメールはすでに存在します") 
+      end
+
+
+
+      it "passwordがない場合は登録できないこと" do
+        @user.password = nil
+        @user.valid?
+        expect(@user.errors[:password]).to include("を入力してください")
+      end
+
+      it 'password半角混合でない場合は登録できないこと' do
+        @user.password = 'aaaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("パスワードは英字と数字の両方を含めて設定してください")
+      end
+
+
+      it 'encrypted_password半角混合でない場合は登録できないこと' do
+        @user.password = 'aaaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("パスワードは英字と数字の両方を含めて設定してください")
+      end
+
+
+
+      
+      
+      it "last_nameがない場合は登録できないこと" do
+        @user.last_name = nil
+        @user.valid?
+        expect(@user.errors[:last_name]).to include("を入力してください")
+      end
+
+      it "last_nameが全角文字でない場合は登録できないこと" do
+        @user.last_name = 'nakamura'
+        @user.valid?
+        expect(@user.errors[:last_name]).to include("は全角文字を使用してください")
+      end
+
+      it "last_name_kanaがない場合は登録できないこと" do
+        @user.last_name_kana = nil
+        @user.valid?
+        expect(@user.errors[:last_name_kana]).to include("を入力してください")
+      end
+
+      it "last_name_kanaがカタカナでない場合は登録できないこと" do
+        @user.last_name_kana = '中むら'
+        @user.valid?
+        expect(@user.errors[:last_name_kana]).to include("は全角カナを使用してください")
+      end
+
+
+
+      it "first_nameがない場合は登録できないこと" do
+        @user.first_name = nil
+        @user.valid?
+        expect(@user.errors[:first_name]).to include("を入力してください")
+      end
+      
+      it "first_nameが全角文字でない場合は登録できないこと" do
+        @user.first_name = 'taichi'
+        @user.valid?
+        expect(@user.errors[:first_name]).to include("は全角文字を使用してください")
+      end
+
+      it "first_name_kanaがない場合は登録できないこと" do
+        @user.first_name_kana = nil
+        @user.valid?
+        expect(@user.errors[:first_name_kana]).to include("を入力してください")
+      end
+
+      it "first_name_kanaがカタカナでない場合は登録できないこと" do
+        @user.first_name_kana ='中むら'
+        @user.valid?
+        expect(@user.errors[:first_name_kana]).to include("は全角カナを使用してください")
+      end
+
+
+
+
+      it "birth_dayがない場合は登録できないこと" do
+        @user.birth_day = nil
+        @user.valid?
+        expect(@user.errors[:birth_day]).to include("を入力してください")
+      end
+end
