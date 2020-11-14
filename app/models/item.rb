c9cd15aaae17048ab1740e3aeb715d5ddc24a0cd
nakamurataiti <67047092+nakamurataiti@users.noreply.github.com>
2020-11-15 02:22:02 +0900

    ActiveStorageとActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :cost
+  belongs_to_active_hash :day
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :status
 end
