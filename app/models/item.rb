4ffd43b884b3c0d62a20ea1dc26512826397047a
nakamurataiti <67047092+nakamurataiti@users.noreply.github.com>
2020-11-15 02:46:42 +0900

    バリデーション作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,27 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :cost
   belongs_to_active_hash :day
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :status
+
+  with_options presence: true do
+    validates :title
+    validates :user_id
+    validates :text
+  end
+  
+  with_options presence: true, format: { with: /\A[0-9]+\z/, message: 'は半角数字を使用してください' } do
+    validates :price
+  end
+
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :cost_id, numericality: { other_than: 1 } 
+  validates :days_id, numericality: { other_than: 1 } 
+  validates :prefecture_id, numericality: { other_than: 1 } 
+  validates :genre_id, numericality: { other_than: 1 } 
 end
