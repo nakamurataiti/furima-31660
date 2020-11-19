68f19f36e213b557ef7ab7289948750124264b8d
nakamurataiti <67047092+nakamurataiti@users.noreply.github.com>
2020-11-19 14:56:28 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,:move_to_index, except: [:index]
+  before_action :set_item, only: [:edit, :show, :update]
 
 
   def index
@@ -16,7 +17,6 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
     if @item.valid?
       @item.save
       redirect_to root_path
@@ -26,24 +26,24 @@ class ItemsController < ApplicationController
   end
   
     def edit
-      @item = Item.find(params[:id])
     end
 
     def update
-      @item = Item.find(params[:id])
-      @item.update(item_params)
-      if @item.save
+      if @item.update(item_params)
         redirect_to item_path
       else
         render :edit
       end
-    end
 
 
   private
 
   def item_params
     params.require(:item).permit(:title, :price, :image, :text, :category_id, :cost_id, :days_id, :prefecture_id, :status_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
   def move_to_index
