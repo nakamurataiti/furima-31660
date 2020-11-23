a9e756068433a6fc25e018b906c6d6c182760feb
nakamurataiti <67047092+nakamurataiti@users.noreply.github.com>
2020-11-23 14:56:09 +0900

    修正３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -55,7 +55,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in? && current_user.id == @item.user_id
+    if @item.order.present? || current_user.id != @item.user_id
       redirect_to action: :index
     end
   end
